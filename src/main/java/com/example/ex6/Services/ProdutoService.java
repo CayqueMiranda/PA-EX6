92e885b9476e0cad5447dbb1199d9f88fdc112a0
CayqueMiranda <cayquemiranda2020@gmail.com>
2026-03-22 15:48:21 -0300

    ProdutoService adicionado

---
+++ src/main/java/com/example/ex6/Services/ProdutoService.java
@@ -0,0 +1,31 @@
+package com.example.ex6.Services;
+
+import com.example.ex6.Models.ProdutoModel;
+import com.example.ex6.Repositories.ProdutoRepository;
+
+import java.util.List;
+
+public class ProdutoService {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public ProdutoModel criarProduto(ProdutoModel produtoModel) {
+        return produtoRepository.save(produtoModel);
+    }
+
+    public List<ProdutoModel> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public ProdutoModel buscarProdutoPorId(Long id) {
+        return produtoRepository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+    }
+
+    public void deletarProduto(Long id) {
+        produtoRepository.deleteById(id);
+    }
+}
